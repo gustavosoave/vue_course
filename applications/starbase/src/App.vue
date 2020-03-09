8ee8de1e2272ca8c25496e6e49f8ee3c80f7ee53
unknown <gustavosoave@gmail.com>
2020-03-08 22:46:42 -0300

    Starbase first API

---
+++ applications/starbase/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    {{title}}
+    <button class="btn btn-primary" @click="fecthCharacter">Test</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      title: "Generate your team"
+    };
+  },
+  methods: {
+    fecthCharacter() {
+      fetch("https://swapi.co/api/people/?format=json", {
+        method: "GET",
+      })
+        .then(response => response.json())
+        .then(json => console.log(json))
+    }
+  }
+};
+</script>
